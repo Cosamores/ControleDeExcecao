c5a269c99dc0c715177bdab17fec25e47e616a9e
Diego Cosamores <diegocosamores@gmail.com>
2021-02-03 15:39:37 -0300

    Initial commit

---
+++ ControleDeExcecao/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeExcecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var idade = 35;
+
+            //if (idade == 0)
+            //    throw new Exception("Valor inválido");
+                
+       
+            //Console.WriteLine($"Minha idade é {idade} e eu sou maior de idade");
+
+            try
+            {
+                var idade = 15;
+
+                if (idade == 0)
+                    throw new Exception("Valor inválido");
+
+
+                Console.WriteLine($"Minha idade é {idade} e eu sou maior de idade");
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
